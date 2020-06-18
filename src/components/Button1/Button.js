a3cb9d803f7dd4d89d3b22ef7e682f874f8c79ae
akochemasov <alexander.kochemasov@litota.ru>
2020-06-18 23:18:40 +0400

    CSS модули

---
+++ src/components/Button1/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Button.module.css';
+
+const Button = () => {
+  return (
+    <>
+      <button className={styles.error}>Button 1</button>
+    </>
+  )
+}
+
+export default Button
---
+++ src/components/Button2/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Button.module.css';
+
+const Button2 = () => {
+  return (
+    <>
+      <button className={styles.error}>Button 2</button>
+    </>
+  )
+}
+
+export default Button2
