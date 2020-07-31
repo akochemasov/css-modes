06b755dd9730e80c892fb682fc787609f12c0074
akochemasov <alexander.kochemasov@litota.ru>
2020-07-31 11:07:39 +0400

    Добавил пересечение стилей

--- src/components/ButtonСssModules/ButtonСssModules.js
+++ src/components/ButtonСssModules/ButtonСssModules.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import stylesNew from './ButtonNew.module.css';
 import styles from './Button.module.css';
 
 const ButtonSsModules = () => {
   return (
     <>
-      <button className={styles.error}>Button Modules</button>
+      <button className={`${styles.error} ${stylesNew.error}`}>Button Modules</button>
     </>
   )
 };
